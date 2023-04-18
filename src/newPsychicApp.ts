12ba1db8e87e66936091f3c00562fa64321fdf23
avocadojesus <jgarbey@gmail.com>
2023-04-17 21:06:34 -0700

    sync instead of build

--- src/newPsychicApp.ts
+++ src/newPsychicApp.ts
@@ -61,7 +61,7 @@ export default async function newHowlApp(
   await sspawn(`cd ./${appName} && git add --all && git commit -m 'psychic init'`)
 
   console.log('building project...')
-  await sspawn(`yarn --cwd=${projectPath} build`)
+  await sspawn(`yarn --cwd=${projectPath} sync`)
 
   if (!api) {
     console.log('building client dependencies...')
