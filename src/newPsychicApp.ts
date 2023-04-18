9326a19b385da42eba239589ece40902d4d0dcc3
avocadojesus <jgarbey@gmail.com>
2023-04-17 21:03:34 -0700

    remove uuid check

--- src/newPsychicApp.ts
+++ src/newPsychicApp.ts
@@ -39,22 +39,6 @@ export default async function newHowlApp(
     })
   }
 
-  if (uuids) {
-    fs.unlinkSync(`${projectPath}/src/app/models/user.ts`)
-    fs.cpSync(`${projectPath}/src/app/models/user.uuid.ts`, `${projectPath}/src/app/models/user.ts`)
-    fs.unlinkSync(`${projectPath}/src/app/models/user.uuid.ts`)
-
-    fs.unlinkSync(`${projectPath}/src/db/migrations/1673997146315-create-users.ts`)
-    fs.cpSync(
-      `${projectPath}/src/db/migrations/1673997146315-create-users.uuid.ts`,
-      `${projectPath}/src/db/migrations/1673997146315-create-users.ts`
-    )
-    fs.unlinkSync(`${projectPath}/src/db/migrations/1673997146315-create-users.uuid.ts`)
-  } else {
-    fs.unlinkSync(`${projectPath}/src/app/models/user.uuid.ts`)
-    fs.unlinkSync(`${projectPath}/src/db/migrations/1673997146315-create-users.uuid.ts`)
-  }
-
   fs.writeFileSync(`${projectPath}/.env`, EnvBuilder.build({ appName, env: 'development' }))
   fs.writeFileSync(`${projectPath}/.env.test`, EnvBuilder.build({ appName, env: 'test' }))
 
