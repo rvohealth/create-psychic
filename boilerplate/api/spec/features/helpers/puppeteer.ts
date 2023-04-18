1308e05fd8870ace1cf10553be8be4bb7c4c0c52
avocadojesus <jgarbey@gmail.com>
2023-04-17 21:00:55 -0700

    cleanup boilerplate round 1

--- boilerplate/api/spec/features/helpers/puppeteer.ts
+++
@@ -1,6 +0,0 @@
-import clientConfig from 'conf/client'
-
-export async function visit(uri: string) {
-  const conf = await clientConfig()
-  await page.goto(`${conf.host}/${uri.replace(/^\//, '')}`)
-}
---
+++ boilerplate/api/src/db/schema.ts
@@ -0,0 +1,12 @@
+import { DateTime } from 'luxon'
+import type { ColumnType } from 'kysely'
+
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>
+
+export type Timestamp = ColumnType<DateTime>
+
+export interface DB {}
+
+export const DBColumns = {}
