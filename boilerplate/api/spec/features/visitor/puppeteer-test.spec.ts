783f64b3fef32e825efab8124774aa7e35ed7ce9
avocadojesus <jgarbey@gmail.com>
2023-04-19 14:15:08 -0700

    simplify boilerplate for spec

--- boilerplate/api/spec/features/visitor/puppeteer-test.spec.ts
+++ boilerplate/api/spec/features/visitor/puppeteer-test.spec.ts
@@ -1,11 +1,4 @@
-import { PsychicServer } from 'psychic'
-const server = new PsychicServer()
-
 describe('ensure puppeteer is working', () => {
-  beforeAll(async () => {
-    await server.boot()
-  })
-
   it('accepts the request', async () => {
     await page.goto('http://localhost:3000')
     await expect(page).toMatchTextContent('hello: world')
