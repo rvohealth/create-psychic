0bc8ad935a4ab24816fc34c9c9d21bbe7a8c5289
avocadojesus <jgarbey@gmail.com>
2023-04-20 10:30:14 -0700

    restore new functionality

--- src/newPsychicApp.ts
+++ src/newPsychicApp.ts
@@ -24,112 +24,112 @@ export default async function newHowlApp(
 ) {
   log.clear()
   log.write(logo() + '\n\n', { cache: true })
-  // log.write(c.magentaBright(`Installing psychic framework to ./${appName}`), { cache: true })
-  // log.write(c.blue(`Step 1. writing boilerplate to ${appName}...`))
-  //   let projectPath: string
-  //   let rootPath = `./${appName}`
-  //   if (api) {
-  //     projectPath = `./${appName}`
-  //     copyRecursive(__dirname + '/../boilerplate/api', `./${appName}`)
-  //   } else {
-  //     projectPath = `./${appName}/api`
-  //     copyRecursive(__dirname + '/../boilerplate', `./${appName}`)
-  //   }
+  log.write(c.magentaBright(`Installing psychic framework to ./${appName}`), { cache: true })
+  log.write(c.blue(`Step 1. writing boilerplate to ${appName}...`))
+  let projectPath: string
+  let rootPath = `./${appName}`
+  if (api) {
+    projectPath = `./${appName}`
+    copyRecursive(__dirname + '/../boilerplate/api', `./${appName}`)
+  } else {
+    projectPath = `./${appName}/api`
+    copyRecursive(__dirname + '/../boilerplate', `./${appName}`)
+  }
 
-  //   log.restoreCache()
-  //   log.write(c.blue(`Step 1. write boilerplate to ${appName}: Done!`), { cache: true })
-  //   log.write(c.blueBright(`Step 2. building default config files...`))
-  //   fs.writeFileSync(`${projectPath}/.env`, EnvBuilder.build({ appName, env: 'development' }))
-  //   fs.writeFileSync(`${projectPath}/.env.test`, EnvBuilder.build({ appName, env: 'test' }))
+  log.restoreCache()
+  log.write(c.blue(`Step 1. write boilerplate to ${appName}: Done!`), { cache: true })
+  log.write(c.blueBright(`Step 2. building default config files...`))
+  fs.writeFileSync(`${projectPath}/.env`, EnvBuilder.build({ appName, env: 'development' }))
+  fs.writeFileSync(`${projectPath}/.env.test`, EnvBuilder.build({ appName, env: 'test' }))
 
-  //   fs.writeFileSync(
-  //     projectPath + '/src/conf/app.yml',
-  //     ConfBuilder.buildAll({
-  //       api,
-  //       ws,
-  //       redis,
-  //       uuids,
-  //     })
-  //   )
+  fs.writeFileSync(
+    projectPath + '/src/conf/app.yml',
+    ConfBuilder.buildAll({
+      api,
+      ws,
+      redis,
+      uuids,
+    })
+  )
 
-  //   log.restoreCache()
-  //   log.write(c.blueBright(`Step 2. build default config files: Done!`), { cache: true })
-  //   log.write(c.cyan(`Step 3. Installing psychic dependencies...`))
-  //   await sspawn(`cd ${projectPath} && yarn install`)
+  log.restoreCache()
+  log.write(c.blueBright(`Step 2. build default config files: Done!`), { cache: true })
+  log.write(c.cyan(`Step 3. Installing psychic dependencies...`))
+  await sspawn(`cd ${projectPath} && yarn install`)
 
-  //   // sleeping here because yarn has a delayed print that we need to clean up
-  //   await sleep(1000)
+  // sleeping here because yarn has a delayed print that we need to clean up
+  await sleep(1000)
 
-  //   log.restoreCache()
-  //   log.write(c.cyan(`Step 3. Install psychic dependencies: Done!`), { cache: true })
-  //   log.write(c.cyanBright(`Step 4. Initializing git repository...`))
-  //   await sspawn(`cd ./${appName} && git init`)
-  //   await sspawn(`cd ./${appName} && git add --all && git commit -m 'psychic init'`)
+  log.restoreCache()
+  log.write(c.cyan(`Step 3. Install psychic dependencies: Done!`), { cache: true })
+  log.write(c.cyanBright(`Step 4. Initializing git repository...`))
+  await sspawn(`cd ./${appName} && git init`)
+  await sspawn(`cd ./${appName} && git add --all && git commit -m 'psychic init'`)
 
-  //   log.restoreCache()
-  //   log.write(c.cyanBright(`Step 4. Initialize git repository: Done!`), { cache: true })
-  //   log.write(c.greenBright(`Step 5. Building project...`))
-  //   await sspawn(`yarn --cwd=${projectPath} dream sync:existing`)
-  //   await sspawn(`yarn --cwd=${projectPath} sync`)
+  log.restoreCache()
+  log.write(c.cyanBright(`Step 4. Initialize git repository: Done!`), { cache: true })
+  log.write(c.greenBright(`Step 5. Building project...`))
+  await sspawn(`yarn --cwd=${projectPath} dream sync:existing`)
+  await sspawn(`yarn --cwd=${projectPath} sync`)
 
-  //   if (!api) {
-  //     await sspawn(`yarn --cwd=${rootPath}/client install`)
-  //   }
+  if (!api) {
+    await sspawn(`yarn --cwd=${rootPath}/client install`)
+  }
 
-  //   log.restoreCache()
-  //   log.write(c.greenBright(`Step 5. Build project: Done!`), { cache: true })
-  //   const helloMessage = `
-  // ${c.greenBright(
-  //   c.bold(
-  //     c.italic(
-  //       `Welcome to Psychic! What does your fortune hold?\ncd into ${c.magentaBright(appName)} to find out!`
-  //     )
-  //   )
-  // )}
+  log.restoreCache()
+  log.write(c.greenBright(`Step 5. Build project: Done!`), { cache: true })
+  const helloMessage = `
+  ${c.greenBright(
+    c.bold(
+      c.italic(
+        `Welcome to Psychic! What does your fortune hold?\ncd into ${c.magentaBright(appName)} to find out!`
+      )
+    )
+  )}
 
-  // ${c.magenta(`to create a database,`)}
-  //   $ psy db:create
-  //   $ NODE_ENV=test psy db:create
+  ${c.magenta(`to create a database,`)}
+    $ psy db:create
+    $ NODE_ENV=test psy db:create
 
-  // ${c.magentaBright(`to migrate a database,`)}
-  //   $ psy db:migrate
-  //   $ NODE_ENV=test psy db:migrate
+  ${c.magentaBright(`to migrate a database,`)}
+    $ psy db:migrate
+    $ NODE_ENV=test psy db:migrate
 
-  // ${c.redBright(`to rollback a database,`)}
-  //   $ psy db:rollback
-  //   $ NODE_ENV=test psy db:rollback
+  ${c.redBright(`to rollback a database,`)}
+    $ psy db:rollback
+    $ NODE_ENV=test psy db:rollback
 
-  // ${c.blueBright(`to drop a database,`)}
-  //   $ psy db:drop
-  //   $ NODE_ENV=test psy db:drop
+  ${c.blueBright(`to drop a database,`)}
+    $ psy db:drop
+    $ NODE_ENV=test psy db:drop
 
-  // ${c.green(`to create a resource (model, migration, serializer, and controller)`)}
-  //   $ psy g:resource user-profile user:belongs_to likes_chalupas:boolean some_id:uuid
+  ${c.green(`to create a resource (model, migration, serializer, and controller)`)}
+    $ psy g:resource user-profile user:belongs_to likes_chalupas:boolean some_id:uuid
 
-  //   # NOTE: doing it this way, you will still need to
-  //   # plug the routes manually in your conf/routes.ts file
+    # NOTE: doing it this way, you will still need to
+    # plug the routes manually in your conf/routes.ts file
 
-  // ${c.greenBright(`to create a model`)}
-  //   $ psy g:model user-profile user:belongs_to likes_chalupas:boolean some_id:uuid
+  ${c.greenBright(`to create a model`)}
+    $ psy g:model user-profile user:belongs_to likes_chalupas:boolean some_id:uuid
 
-  // ${c.yellow(`to create a migration`)}
-  //   $ psy g:migration create-user-profiles
+  ${c.yellow(`to create a migration`)}
+    $ psy g:migration create-user-profiles
 
-  // ${c.yellowBright(`to start a dev server at localhost:7777,`)}
-  //   $ psy dev
+  ${c.yellowBright(`to start a dev server at localhost:7777,`)}
+    $ psy dev
 
-  // ${c.magentaBright(`to run unit tests,`)}
-  //   $ psy uspec
+  ${c.magentaBright(`to run unit tests,`)}
+    $ psy uspec
 
-  // ${c.magentaBright(`to run feature tests,`)}
-  //   $ psy fspec
+  ${c.magentaBright(`to run feature tests,`)}
+    $ psy fspec
 
-  // ${c.magentaBright(`to run unit tests, and then if they pass, run feature tests,`)}
-  //   $ psy spec
+  ${c.magentaBright(`to run unit tests, and then if they pass, run feature tests,`)}
+    $ psy spec
 
-  // # NOTE: before you get started, be sure to visit your ${c.magenta('.env')} and ${c.magenta('.env.test')}
-  // # files and make sure they have database credentials set correctly.
-  // # you can see conf/dream.ts to see how those credentials are used.
-  //   `
-  //   console.log(helloMessage)
+  # NOTE: before you get started, be sure to visit your ${c.magenta('.env')} and ${c.magenta('.env.test')}
+  # files and make sure they have database credentials set correctly.
+  # you can see conf/dream.ts to see how those credentials are used.
+    `
+  console.log(helloMessage)
 }
