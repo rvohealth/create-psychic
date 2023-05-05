e7f75b5c81fea2d8e1248d6915840ab16a3c804f
avocadojesus <jgarbey@gmail.com>
2023-05-04 21:11:21 -0700

    fix snakeify

--- dist/envBuilder.js
+++ dist/envBuilder.js
@@ -16,10 +16,14 @@ APP_ENCRYPTION_KEY='${(0, generateEncryptionKey_1.default)()}'
     }
 }
 exports.default = EnvBuilder;
+// TODO: import from shared space. The version within dream contains the most robust variant of snakeify,
+// though we don't really use it for anything other than string transformations, so this version has been simplified.
 function snakeify(str) {
     return str
         .replace(/(?:^|\.?)([A-Z])/g, (_, y) => '_' + y.toLowerCase())
         .replace(/^_/, '')
+        .replace(/\//g, '_')
+        .replace(/-/g, '_')
         .toLowerCase();
 }
 //# sourceMappingURL=envBuilder.js.map
--- src/envBuilder.ts
+++ src/envBuilder.ts
@@ -12,9 +12,13 @@ APP_ENCRYPTION_KEY='${generateEncryptionKey()}'
   }
 }
 
-function snakeify(str: any): string {
+// TODO: import from shared space. The version within dream contains the most robust variant of snakeify,
+// though we don't really use it for anything other than string transformations, so this version has been simplified.
+function snakeify(str: any): any {
   return str
     .replace(/(?:^|\.?)([A-Z])/g, (_: string, y: string) => '_' + y.toLowerCase())
     .replace(/^_/, '')
+    .replace(/\//g, '_')
+    .replace(/-/g, '_')
     .toLowerCase()
 }
