7f32a670b312cda6b7dcf6f426d0921dd8a5ca5f
avocadojesus <jgarbey@gmail.com>
2023-05-08 13:26:52 -0700

    update reply boilerplate, update psychic

--- boilerplate/api/src/conf/repl.ts
+++ boilerplate/api/src/conf/repl.ts
@@ -1,20 +1,16 @@
-import * as dotenv from 'dotenv'
+import './loadEnv'
 import * as repl from 'node:repl'
 import * as fs from 'fs/promises'
 import * as path from 'path'
-
-dotenv.config({ path: '.env' })
+import { Dream, pascalize } from 'psychic'
 
 const replServer = repl.start('> ')
 export default (async function () {
   const dreamPaths = (await getFiles('./src/app/models')).filter(file => /\.ts$/.test(file))
   for (const dreamPath of dreamPaths) {
     const importablePath = dreamPath.replace(/.*\/src/, '..')
-
-    if (isImportable(importablePath)) {
-      const DreamClass = (await import(importablePath)).default
-      replServer.context[await globalName(DreamClass, importablePath)] = DreamClass
-    }
+    const dreamClass = (await import(importablePath)).default as typeof Dream
+    replServer.context[await dreamClass.globalName()] = dreamClass
   }
 })()
 
@@ -27,21 +23,4 @@ async function getFiles(dir: string): Promise<string[]> {
     })
   )
   return Array.prototype.concat(...files)
-}
-
-let pascalize: any
-async function globalName(dreamClassOrIndexFile: any, importablePath: string) {
-  // need to import from psychic dynamically, or else database
-  // credentials get messed up.
-  if (!pascalize) pascalize = (await import('psychic')).pascalize
-
-  if (dreamClassOrIndexFile?.isDream) return (dreamClassOrIndexFile as any).name
-  else {
-    const paths = importablePath.split('/')
-    return pascalize(paths[paths.length - 2])
-  }
-}
-
-function isImportable(file: string) {
-  return file.split('/').length === 4 || (file.split('/').length === 5 && /index.ts$/.test(file))
 }
