2330c0638a89bcf9ea1e13480cf0c1795252ad68
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2023-05-31 13:01:41 -0500

    Leave bootstrapped schema and associations to Dream

--- boilerplate/api/src/db/associations.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  __placeholder__: {},
-}
-export interface SyncedAssociations {
-  __placeholder__: {}
-}
-export interface SyncedBelongsToAssociations {
-  __placeholder__: {}
-}
