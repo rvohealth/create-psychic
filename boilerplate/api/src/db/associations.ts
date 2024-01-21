61d5e264838ed88f6a75555cbadad8e39a84de69
avocadojesus <jgarbey@gmail.com>
2024-01-20 16:34:50 -0800

    add boilerplate schema

---
+++ boilerplate/api/src/db/associations.ts
@@ -0,0 +1,5 @@
+export default {}
+
+export interface SyncedAssociations {}
+
+export interface VirtualColumns {}
---
+++ boilerplate/api/src/db/schema.ts
@@ -0,0 +1,11 @@
+export interface DB {}
+
+export class DBClass {}
+
+export interface InterpretedDB {}
+
+export class InterpretedDBClass {}
+
+export const DBColumns = {}
+
+export const DBTypeCache = {} as Partial<Record<keyof DB, any>>
