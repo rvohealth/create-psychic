d59f4598e94236cdc935249092b3bdc47ce4ad25
avocadojesus <jgarbey@gmail.com>
2024-01-20 16:10:37 -0800

    bring build up to date

---
+++ boilerplate/api/src/app/models/ApplicationModel.ts
@@ -0,0 +1,34 @@
+import { Dream, Dreamconf } from '@rvohealth/dream'
+import { AllColumns, DBClass, DBColumns, DBTypeCache, InterpretedDBClass } from '../../db/schema'
+import SyncedAssociationsVal, {
+  SyncedAssociations,
+  SyncedBelongsToAssociations,
+  VirtualColumns,
+} from '../../db/associations'
+import dreamconf from '../../conf/dreamconf'
+
+export default class ApplicationModel extends Dream {
+  public get DB() {
+    return new DBClass()
+  }
+
+  public get syncedAssociations(): SyncedAssociations {
+    return SyncedAssociationsVal as SyncedAssociations
+  }
+
+  public get allColumns(): typeof AllColumns {
+    return AllColumns as typeof AllColumns
+  }
+
+  public get dreamconf(): Dreamconf<
+    DBClass,
+    InterpretedDBClass,
+    SyncedAssociations,
+    SyncedBelongsToAssociations,
+    VirtualColumns,
+    typeof DBColumns,
+    typeof DBTypeCache
+  > {
+    return dreamconf
+  }
+}
