2899a3daee4561c5a752e3f1c227b26b657a57fa
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2024-02-14 13:38:05 -0600

    Best practices from real apps TS_SAFE and APP_ROOT_PATH necessary for feature spec runs since jest runs in different process than the server

--- boilerplate/api/spec/features/setup/hooks.ts
+++ boilerplate/api/spec/features/setup/hooks.ts
@@ -2,6 +2,9 @@ import 'expect-puppeteer'
 import * as dotenv from 'dotenv'
 import { truncate } from '@rvohealth/dream/spec-helpers'
 
+process.env.APP_ROOT_PATH = process.cwd()
+process.env.TS_SAFE = '1'
+
 dotenv.config({ path: '.env.test' })
 
 beforeEach(async () => {
