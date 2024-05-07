e608143e184c720a2c15abeca0334225899a0da9
avocadojesus <jgarbey@gmail.com>
2024-05-06 21:48:13 -0700

    use playwright instead of puppeteer

--- boilerplate/api/spec/unit/setup/hooks.ts
+++ boilerplate/api/spec/unit/setup/hooks.ts
@@ -1,5 +1,5 @@
 import '../../../src/conf/loadEnv'
-import 'psychic/spec-helpers'
+import '@rvohealth/psychic/spec-helpers'
 import 'expect-puppeteer'
 import { truncate } from '@rvohealth/dream/spec-helpers'
 
