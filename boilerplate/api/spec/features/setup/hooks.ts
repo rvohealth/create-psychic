4a2efcc7b1df189af7f9a8bc9d1d74e202c6c4dd
avocadojesus <jgarbey@gmail.com>
2024-05-06 23:09:21 -0700

    remove remaining puppeteer lines from code

--- boilerplate/api/spec/features/setup/hooks.ts
+++ boilerplate/api/spec/features/setup/hooks.ts
@@ -1,12 +1,14 @@
-import 'expect-puppeteer'
-import * as dotenv from 'dotenv'
+import '../../../src/conf/global'
 import { truncate } from '@rvohealth/dream/spec-helpers'
 
 process.env.APP_ROOT_PATH = process.cwd()
 process.env.TS_SAFE = '1'
 
-dotenv.config({ path: '.env.test' })
+jest.setTimeout(
+  (process.env.JEST_FEATURE_TIMEOUT_SECONDS && parseInt(process.env.JEST_FEATURE_TIMEOUT_SECONDS) * 1000) ||
+    25000
+)
 
 beforeEach(async () => {
   await truncate()
-})
+}, 15000)
--- boilerplate/api/spec/features/visitor/example-feature-spec.spec.ts
+++ boilerplate/api/spec/features/visitor/example-feature-spec.spec.ts
@@ -1,5 +1,5 @@
-describe('puppeteer sample test', () => {
-  it('my first puppeteer test', async () => {
+describe('playwright sample test', () => {
+  it('my first playwright test', async () => {
     await page.goto('http://localhost:3000')
     // TODO add specs here for your client app
   })
