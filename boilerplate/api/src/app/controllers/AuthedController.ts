214fb17be93857ff46a32c9e5d914ee95859c9ff
avocadojesus <jgarbey@gmail.com>
2024-05-07 22:02:25 -0700

    automatically generate authed controllers with basic authentication patterns

--- boilerplate/api/src/app/controllers/AuthedController.ts
+++ boilerplate/api/src/app/controllers/AuthedController.ts
@@ -1,8 +1,16 @@
 import { PsychicController, BeforeAction } from '@rvohealth/psychic'
 
 export default class AuthedController extends PsychicController {
+  // protected currentUser: User
   @BeforeAction()
   public async authenticate() {
     throw `TODO: Implement authentication scheme!`
+    // implement an authentication pattern that ends with you setting
+    // this.currentUser to a user. i.e.
+    //
+    // const token = this.req.headers['token']
+    // const userId = await Encrypt.decode(token)
+    // this.currentUser = await User.find(userId)
+    //
   }
 }
