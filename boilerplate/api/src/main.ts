ad36557949b9e8255692683121d265fa6a389da9
avocadojesus <jgarbey@gmail.com>
2024-10-25 23:32:10 -0700

    add new yarn to gitignore

--- boilerplate/api/src/main.ts
+++ boilerplate/api/src/main.ts
@@ -8,13 +8,6 @@ async function start() {
 
   const server = new PsychicServer()
   await server.start()
-
-  process.on('SIGINT', () => {
-    server
-      .stop()
-      .then(() => {})
-      .catch(() => {})
-  })
 }
 
 void start()
