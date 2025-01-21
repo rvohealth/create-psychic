bfb0a3b26fc4075b2f8101faed94428e252a961c
avocadojesus <jgarbey@gmail.com>
2025-01-21 15:40:24 -0800

    add AppEnv to boilerplate, globally to repl
    close https://rvohealth.atlassian.net/browse/PDTC-6745

---
+++ boilerplate/api/src/conf/AppEnv.ts
@@ -0,0 +1,16 @@
+import { Env } from '@rvohealth/dream'
+
+class AppEnvClass extends Env<{
+  boolean: 'REQUEST_LOGGING'
+  integer: 'BG_JOBS_REDIS_PORT' | 'WS_REDIS_PORT'
+  string:
+    | 'DOMAIN'
+    | 'BG_JOBS_REDIS_HOST'
+    | 'BG_JOBS_REDIS_USERNAME'
+    | 'BG_JOBS_REDIS_PASSWORD'
+    | 'WS_REDIS_HOST'
+    | 'CORS_HOSTS'
+}> {}
+
+const AppEnv = new AppEnvClass()
+export default AppEnv
--- boilerplate/api/src/conf/repl.ts
+++ boilerplate/api/src/conf/repl.ts
@@ -2,9 +2,14 @@ import './loadEnv'
 import * as repl from 'node:repl'
 import { loadRepl } from '@rvohealth/dream'
 import initializePsychicApplication from '../cli/helpers/initializePsychicApplication'
+import AppEnv from './AppEnv'
 
 const replServer = repl.start('> ')
+
 export default (async function () {
   await initializePsychicApplication()
+
+  replServer.context.AppEnv = AppEnv
+
   loadRepl(replServer.context)
 })()
