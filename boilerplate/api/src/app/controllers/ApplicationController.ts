6c723ee06af47cf602eeef4a267399c86340cade
avocadojesus <jgarbey@gmail.com>
2025-02-26 08:46:21 -0800

    update boilerplate for multiple openapi specs
    close https://rvohealth.atlassian.net/browse/PDTC-7192

--- boilerplate/api/src/app/controllers/ApplicationController.ts
+++ boilerplate/api/src/app/controllers/ApplicationController.ts
@@ -1,8 +1,12 @@
-import { PsychicController } from '@rvohealth/psychic'
+import { PsychicController, PsychicOpenapiControllerConfig } from '@rvohealth/psychic'
 import psychicTypes from '../../types/psychic'
 
 export default class ApplicationController extends PsychicController {
   public get psychicTypes() {
     return psychicTypes
+  }
+
+  public static get openapiConfig(): PsychicOpenapiControllerConfig<ApplicationController> {
+    return { names: ['default', 'mobile'] }
   }
 }
