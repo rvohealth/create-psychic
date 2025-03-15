ace543c700e5303370972e1edfeddcceea4a620c
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-03-15 12:01:14 -0500

    Route boilerplate needs a comment for the automatic route insertion for resources to work

--- boilerplate/api/src/conf/routes.ts
+++ boilerplate/api/src/conf/routes.ts
@@ -1,3 +1,5 @@
 import { PsychicRouter } from '@rvoh/psychic'
 
-export default (r: PsychicRouter) => {}
+export default (r: PsychicRouter) => {
+  // add routes here, perhaps by running `yarn psy g:resource v1/pets Pet name:citext birthdate:date species:enum:pet_species:dog,cat,fish`
+}
