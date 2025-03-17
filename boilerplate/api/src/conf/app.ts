f3865f37906c7773277473ccb76c543b67dc7eee
Thomas Hopkins <hopkinsth@gmail.com>
2025-03-17 10:36:18 -0400

    fix(boilerplate): remove escape sequences from JSON log output
    using `colorize()` and `json()` produces output like this
    
    ``` json
    {"level":"\u001b[32minfo\u001b[39m", "message":"GET /helo 200 4ms}
    ```

--- boilerplate/api/src/conf/app.ts
+++ boilerplate/api/src/conf/app.ts
@@ -94,7 +94,7 @@ export default async (psy: PsychicApplication) => {
       app.use(
         expressWinston.logger({
           transports: [new winston.transports.Console()],
-          format: winston.format.combine(winston.format.colorize(), winston.format.json()),
+          format: winston.format.combine(winston.format.json()),
           meta: true, // optional: control whether you want to log the meta data about the request (default to true)
           msg: 'HTTP {{req.method}} {{req.url}}', // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
           expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
