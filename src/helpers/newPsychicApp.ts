1e4c72c1701f92495c4ba3eda37f0f7730915004
avocadojesus <jgarbey@gmail.com>
2025-03-21 11:56:16 -0700

    don't override client when provided explicitly via cli args

--- src/helpers/newPsychicApp.ts
+++ src/helpers/newPsychicApp.ts
@@ -104,8 +104,9 @@ export default async function newPsychicApp(appName: string, options: InitPsychi
       options.adminClient = answer
     }
   } else {
-    options.client = 'none'
-    options.adminClient = 'none'
+    // if they explicitly provide clients, we do not want to override, so we use ||=
+    options.client ||= 'none'
+    options.adminClient ||= 'none'
   }
 
   if (options.workers === undefined) {
@@ -128,6 +129,7 @@ export default async function newPsychicApp(appName: string, options: InitPsychi
 
   const rootPath = `./${appName}`
   const hasClient = options.client !== 'none' || options.adminClient !== 'none'
+  console.log({ options, hasClient })
   const projectPath = hasClient ? path.join(process.cwd(), appName, 'api') : path.join(process.cwd(), appName)
 
   if (hasClient) {
