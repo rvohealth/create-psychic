ef60e0e4907341ef6c6e9434f76dea1388a6e3a2
avocadojesus <jgarbey@gmail.com>
2025-03-25 14:46:09 -0700

    bump dream, make authentication helper async

--- boilerplate/api/spec/unit/helpers/authentication.ts
+++ boilerplate/api/spec/unit/helpers/authentication.ts
@@ -1,7 +1,7 @@
 import { specRequest } from '@rvoh/psychic-spec-helpers'
 import User from '../../../src/app/models/User.js'
 
-export function addEndUserAuthHeader(request: typeof specRequest, user: User, headers: object) {
+export default async function addEndUserAuthHeader(request: typeof specRequest, user: User, headers: object) {
   // Update this function to either modify headers (e.g. with an Authorization header)
   // or to apply a cookie to the request
   return { ...headers }
