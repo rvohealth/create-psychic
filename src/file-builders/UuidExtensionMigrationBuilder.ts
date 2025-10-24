5f7d2082172f36592d13691a61642bc5e6859913
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-24 10:03:06 -0400

    fix

--- src/file-builders/UuidExtensionMigrationBuilder.ts
+++ src/file-builders/UuidExtensionMigrationBuilder.ts
@@ -2,7 +2,7 @@ export default class UuidExtensionMigrationBuilder {
   public static build() {
     return `\
 import { Kysely } from 'kysely'
-import { DreamMigrationHelpers } from '@rvoh/dream'
+import { DreamMigrationHelpers } from '@rvoh/dream/db'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function up(db: Kysely<any>): Promise<void> {
