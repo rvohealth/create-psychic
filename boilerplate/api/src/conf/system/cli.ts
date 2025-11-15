749ac4486900c0b7c33aefa0a3d6fcc7ee120a3d
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-11-15 10:33:20 -0600

    Boilerplate A.I. config

--- boilerplate/api/src/conf/system/cli.ts
+++ boilerplate/api/src/conf/system/cli.ts
@@ -8,7 +8,9 @@
 import '../loadEnv.js'
 
 import { PsychicCLI } from '@rvoh/psychic/system'
-import { Command } from 'commander'
+import { Argument, Command } from 'commander'
+import * as fs from 'node:fs'
+import * as path from 'node:path'
 import seedDb from '../../db/seed.js'
 import initializePsychicApp from './initializePsychicApp.js'
 
@@ -18,5 +20,39 @@ PsychicCLI.provide(program, {
   initializePsychicApp,
   seedDb,
 })
+
+const SUPPORTED_AI_ENUM_VALUES = ['cursor', 'copilot', 'claude'] as const
+type SUPPORTED_AI_ENUM = (typeof SUPPORTED_AI_ENUM_VALUES)[number]
+const AI_RULES_URL =
+  'https://raw.githubusercontent.com/rvohealth/create-psychic/refs/heads/create-psychic/2.0/ai/rules'
+const AI_FILE_MAP: Record<SUPPORTED_AI_ENUM, string> = {
+  claude: 'CLAUDE.md',
+  copilot: '.github/copilot-instructions.md',
+  cursor: '.cursor/rules',
+}
+
+program
+  .command('sync:ai-rules')
+  .addArgument(new Argument('<target>', 'A.I. target').choices([...SUPPORTED_AI_ENUM_VALUES, 'all']))
+  .description('syncs the ')
+  .action(async (_target: SUPPORTED_AI_ENUM | 'all') => {
+    const targets = _target === 'all' ? SUPPORTED_AI_ENUM_VALUES : [_target]
+    for (const target of targets) {
+      const rules = await (await fetch(AI_RULES_URL)).text()
+      const targetPath = path.join(process.cwd(), AI_FILE_MAP[target])
+      const originalCurrentFileContents = fs.existsSync(targetPath)
+        ? fs.readFileSync(targetPath).toString()
+        : ''
+      const currentFileContents = originalCurrentFileContents.split(`\
+//////////////////////////////////////////////
+// end:OFFICIAL PSYCHIC RULES DO NOT MODIFY //
+//////////////////////////////////////////////
+`)
+      const customRules = currentFileContents.length > 1 ? currentFileContents.at(-1)! : ''
+      fs.writeFileSync(targetPath, rules + customRules)
+    }
+
+    process.exit()
+  })
 
 program.parse(process.argv)
