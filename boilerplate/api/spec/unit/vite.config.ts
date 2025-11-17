00de9b1c4a3fda009a5ddd659241846882a94f9d
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-11-17 10:54:37 -0600

    Fix Puppeteer page type Dream and Psychic 2.0.1

--- boilerplate/api/spec/unit/vite.config.ts
+++ boilerplate/api/spec/unit/vite.config.ts
@@ -22,7 +22,6 @@ export default defineConfig({
     fileParallelism: true,
     maxConcurrency: parseInt(process.env.DREAM_PARALLEL_TESTS || '1'),
     maxWorkers: parseInt(process.env.DREAM_PARALLEL_TESTS || '1'),
-    minWorkers: 1,
     watch: false,
     mockReset: true,
     clearMocks: true,
