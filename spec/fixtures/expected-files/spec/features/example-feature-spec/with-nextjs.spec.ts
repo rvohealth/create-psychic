67a3b553141dd6a7de8d7cab56fe32aa95270f1c
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-03-25 18:40:51 -0500

    More explicit front end app environment names

--- spec/fixtures/expected-files/spec/features/example-feature-spec/with-nextjs.spec.ts
+++ spec/fixtures/expected-files/spec/features/example-feature-spec/with-nextjs.spec.ts
@@ -3,7 +3,7 @@ import { visit } from '@rvoh/psychic-spec-helpers'
 
 describe('puppeteer sample test', () => {
   it('my first puppeteer test', async () => {
-    await visit('/', { baseUrl: AppEnv.string('CLIENT_HOST') })
+    await visit('/', { baseUrl: AppEnv.string('CLIENT_APP_HOST') })
     await expect(page).toMatchTextContent('get started')
   })
 })
