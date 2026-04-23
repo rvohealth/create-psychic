a2d5bd6faa9bcbb56114a40c1e22c9d19a3b81b9
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-04-23 09:37:20 -0700

    Scaffold websocket auth via resolveWebsocketUser helper (R-013)

--- boilerplate/api/src/conf/initializers/websockets.ts
+++ boilerplate/api/src/conf/initializers/websockets.ts
@@ -1,9 +1,9 @@
 import AppEnv from '@conf/AppEnv.js'
 import allowedCorsOrigins from '@conf/system/allowedCorsOrigins.js'
+import resolveWebsocketUser from '@conf/system/resolveWebsocketUser.js'
 import { PsychicApp } from '@rvoh/psychic'
-import { PsychicAppWebsockets } from '@rvoh/psychic-websockets'
+import { PsychicAppWebsockets, Ws } from '@rvoh/psychic-websockets'
 import { Redis } from 'ioredis'
-// import User from '@models/User.js'
 
 export default (psy: PsychicApp) => {
   if (AppEnv.serviceRole !== 'websockets' && !AppEnv.isTest) return
@@ -48,20 +48,14 @@ function initializeWebsockets(wsApp: PsychicAppWebsockets) {
   // ******
 
   wsApp.on('ws:start', io => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     io.of('/').on('connection', async socket => {
-      // below is an example of how you might connect to websockets
-      // const token = socket.handshake.auth.token as string
-      // const userId = Encrypt.decrypt<string>(token, {
-      //   algorithm: 'aes-256-gcm',
-      //   key: AppEnv.string('APP_ENCRYPTION_KEY'),
-      // })!
-      // const user = await User.find(userId)
-      //
-      // if (user) {
-      //   // this automatically fires the /ops/connection-success message
-      //   await Ws.register(socket, user.id)
-      // }
+      const user = await resolveWebsocketUser(socket)
+      if (!user) {
+        socket.disconnect(true)
+        return
+      }
+      // this automatically fires the /ops/connection-success message
+      await Ws.register(socket, user)
     })
   })
 
